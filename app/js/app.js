9fb012379b7b02f0295d4c4986c7bb78aa1dc1cf
Germesych <kaiot1984@gmail.com>
2021-03-22 21:59:37 +0300

    v 0.02

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,21 @@
-// // Import jQuery module (npm i jquery)
+// Import jQuery module (npm i jquery)
 // import $ from 'jquery'
 // window.jQuery = $
 // window.$ = $
 
-// // Import vendor jQuery plugin example (not module)
+// Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
-
-import Swiper from 'swiper'
-import "./moduls/maps";
-import "./moduls/slider";
+// import "../libs/likely/likely";
+// import "../libs/prognroll/prognroll";
+// import Swiper from 'swiper'
+// import "./moduls/maps";
+// import "./moduls/slider";
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JS it work')
-    const swiperImg = new Swiper('.swiper-container', {
-        loop: false,
-        speed: 2400, //Скорость анимации
-        slidesPerView: 1,
-        spaceBetween: 10,
-    })
+    // const swiperImg = new Swiper('.swiper-container', {
+    //     loop: false,
+    //     speed: 2400, //Скорость анимации
+    //     slidesPerView: 1,
+    //     spaceBetween: 10,
+    // })
 })
